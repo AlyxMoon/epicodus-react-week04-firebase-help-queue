810289b19569d77bc26c04b733884ce2b17b7fe4
AlyxMoon <allisterkmoon@gmail.com>
2021-09-20 11:37:53 -0700

    add firebase to the app

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import rootReducer from './reducers/index';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
 
-const store = createStore(rootReducer);
+import './index.css'
 
+import firebase from './firebase'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+import rootReducer from './reducers/index'
 
+const store = createStore(rootReducer)
+const reduxFirebaseProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...reduxFirebaseProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
